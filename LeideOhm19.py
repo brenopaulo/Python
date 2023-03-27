fc75fd3d5d58ff944dd814481962174eac010b8c
KronostronX <breno111003@gmail.com>
2023-03-27 20:28:08 -0300

    Lei de Ohm 1.9

---
+++ LeideOhm19.py
@@ -0,0 +1,4 @@
+r=int(input('Digite o valor da resistência elétrica: '))
+i=int(input('Digite o valor da corrente elétrica: '))
+U=(r*i)
+print('A tensão desse circuito elétrico é {} volts'.format(U))
