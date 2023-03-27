c693322d51f0c45a1f3ee20e582c4a20b6d79b47
KronostronX <breno111003@gmail.com>
2023-03-27 20:57:11 -0300

    Teste do Triangulo 2.5

---
+++ TestedoTriangulo25.py
@@ -0,0 +1,7 @@
+lado1=float(input('Digite o valor do primeiro lado do triângulo: '))
+lado2=float(input('Digite o valor do segundo lado do triângulo: '))
+lado3=float(input('Digite o valor do terceiro lado do triângulo: '))
+if(lado1+lado2>lado3 and lado2+lado3>lado1 and lado1+lado3>lado2):print('Não é possível formar um triângulo com esses valores')
+elif(lado1==lado2==lado3):print('Esse triângulo se trata de um triângulo equilátero')
+elif(lado1==lado2!=lado3 and lado1!=lado2==lado3 and lado1==lado3!=lado2):print('Esse triângulo se trata de um triângulo isósceles')
+elif(lado1!=lado2!=lado3):print('Esse triângulo se trata de um triângulo escaleno')
