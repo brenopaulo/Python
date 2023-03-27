213ee5487221a82fef3e5d74484d8bb3c57e249c
KronostronX <breno111003@gmail.com>
2023-03-27 20:48:56 -0300

    Terreno Grande 2.1

---
+++ TerrenoGrande21.py
@@ -0,0 +1,6 @@
+base=int(input('Digite o valor da base do retângulo: '))
+altura=int(input('Digite o valor da altura do retângulo: '))
+área=(base*altura)
+áreatg=(base*altura)
+if(área>100):área=('{} .Sendo assim, é um Terreno grande por se tratar de uma área de mais de 100'.format(áreatg))
+print('A área do retângulo é {}'.format(área))
