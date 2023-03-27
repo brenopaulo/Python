cd89218d33d50c1a9eef24356d8e8530bd8cb15e
KronostronX <breno111003@gmail.com>
2023-03-27 20:35:21 -0300

    Volume do Cone 1.14

---
+++ VolumedoCone114.py
@@ -0,0 +1,6 @@
+r=int(input('Digite o valor do raio da base do cone: '))
+h=int(input('Digite o valor da altura do cone: '))
+import math
+x=math.pi
+V=(1/3*h*x*r**2)
+print('O volume do cone é {}'.format(V))
