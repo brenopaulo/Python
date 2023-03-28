c7778d00e1f583015df64c53298480bc5a6603e6
KronostronX <breno111003@gmail.com>
2023-03-27 21:27:13 -0300

    Soma de 1 a 100 2.17

---
+++ SomadeUmaCem217.py
@@ -0,0 +1,13 @@
+i = 2
+s = 1
+print(s,'+')
+soma=s+i
+while i<=99:
+    print(i)
+    i = i + 1
+    s = s + 1
+    print(s,'+')
+    soma=soma+i
+print('A soma dos números de 1 a 100 é: {}'.format(soma))
+
+    
