0c25217426e43ec3a514330f6e8268bedd4b085c
KronostronX <breno111003@gmail.com>
2023-03-27 21:25:58 -0300

    Tabuada de um Numero Positivo 2.16

---
+++ TabuadadeumNumeroPositivo216.py
@@ -0,0 +1,16 @@
+num=float(input('Digite um valor: '))
+if num>0:
+    i = 0
+    while i<=10:
+        print(num,'X',i,'=',num*i)
+        i = i + 1
+else:
+    while num<0:
+        num=float(input('ERROR,Digite um número positivo: '))
+if num>0:
+    i = 0
+    while i<=10:
+        print(num,'X',i,'=',num*i)
+        i = i + 1
+        
+        
