90c1b553b65436d911776a958f15f1f023bbe877
KronostronX <breno111003@gmail.com>
2023-03-27 21:28:26 -0300

    Tabuada de um a Vinte com Pausas

---
+++ TabuadadeUmaVinteComPausas218.py
@@ -0,0 +1,100 @@
+a = 1
+r=input('Pressione qualquer tecla: ')
+while a<=10:
+    print(1,'X',a,'=',a*1)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(2,'X',a,'=',a*2)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(3,'X',a,'=',a*3)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(4,'X',a,'=',a*4)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(5,'X',a,'=',a*5)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(6,'X',a,'=',a*6)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(7,'X',a,'=',a*7)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(8,'X',a,'=',a*8)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(9,'X',a,'=',a*7)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(10,'X',a,'=',a*10)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(11,'X',a,'=',a*11)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(12,'X',a,'=',a*12)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(13,'X',a,'=',a*13)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(14,'X',a,'=',a*14)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(15,'X',a,'=',a*15)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(16,'X',a,'=',a*16)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(17,'X',a,'=',a*17)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(18,'X',a,'=',a*18)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(19,'X',a,'=',a*19)
+    a=a+1
+print('---------------')
+a = 1
+while a<=10:
+    print(20,'X',a,'=',a*20)
+    a=a+1
