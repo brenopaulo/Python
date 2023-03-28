fc4071805b5f61a84c2bb3430b3c853fdf770f6d
KronostronX <breno111003@gmail.com>
2023-03-27 21:04:55 -0300

    Segundo Valor é Maior 2.9

---
+++ SegundoValoréMaior29.py
@@ -0,0 +1,6 @@
+primeiro=float(input('Digite o primeiro valor: '))
+segundo=float(input('Digite o segundo valor: '))
+if(segundo>primeiro):print('O segundo valor é maior')
+if(segundo<primeiro):float(input('Digite outro segundo valor: '))
+if(segundo==primeiro):float(input('Digite outro segundo valor: '))
+
