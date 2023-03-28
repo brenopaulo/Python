1066c792e8d0e95fbc811f304507a7a28b3c4578
KronostronX <breno111003@gmail.com>
2023-03-27 21:06:15 -0300

    Nota para Aprovação 2.10

---
+++ NotaparaAprovação210.py
@@ -0,0 +1,3 @@
+p1=float(input('Digite a sua nota da primeira avaliação: '))
+p2=((15-p1)/2)
+print('Voce irá precisar tirar {} na segunda avaliação para conseguir atingir a média de 5 e ser aprovado'.format(p2))
